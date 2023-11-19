498c9127ebe7c71cf13e40d03c83c59dd185f03c
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2023-11-20 01:44:48 +0200

    Add files via upload

--- api/index.php
+++ api/index.php
@@ -1,7 +1,15 @@
 <?php
+include "./routes.php";
 
-include "content/index.php";
+$url = $_SERVER["REQUEST_URI"];
 
-echo "</br>";
+if ($url == '/api' . $_HOME) {
+    include "./content/index.php";
+} else {
+    $_errorCode = 404;
+    $_errorH = "Page not found";
+    $_errorMsg = "Sorry, we couldn’t find the page you’re looking for.";
+    include "./_error.php";
+}
 
-echo $_SERVER["REQUEST_URI"];
+?>
