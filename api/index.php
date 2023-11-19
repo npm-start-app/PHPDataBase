b62975f14062de0b7abe62e93fb0107f09338ca6
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2023-11-20 01:48:21 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,15 @@
 <?php
-include "/routes.php";
+include "routes.php";
 
 $url = $_SERVER["REQUEST_URI"];
 
 if ($url == $_HOME) {
-    include "/content/index.php";
+    include "content/index.php";
 } else {
     $_errorCode = 404;
     $_errorH = "Page not found";
     $_errorMsg = "Sorry, we couldn’t find the page you’re looking for.";
-    include "/_error.php";
+    include "_error.php";
 }
 
 ?>
