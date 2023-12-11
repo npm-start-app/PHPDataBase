5e2a19ec9179cb889ffccda4c9a62eadc6286440
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2023-12-11 16:55:37 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,6 +6,14 @@ $url = $_SERVER["REQUEST_URI"];
 if ($url == $_HOME) {
     phpinfo();
     include "content/index.php";
+    $redis = new Redis();
+
+    $redis->connect("redis-16067.c302.asia-northeast1-1.gce.cloud.redislabs.com", 16067);
+    $redis->auth('oT07aeSLCCB8d99gIXxnCYmBhcxaHzl7');
+
+    echo $redis->get("admin");
+    
+    $redis->close();
 } else {
     $_errorCode = 404;
     $_errorH = "Page not found";
