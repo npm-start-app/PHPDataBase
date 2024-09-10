ef898276ec1834fd123de1892ba19295ea0777e3
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-10 17:32:34 +0200

    Update main.php

--- api/lib/main.php
+++ api/lib/main.php
@@ -2,15 +2,15 @@
 namespace WBlib;
 $libName = "lib";
 
-include_once('./CustomErrorHandlers/CustomErrorHandler.php');
+include_once(__DIR__ . '/CustomErrorHandlers/CustomErrorHandler.php');
 
-include_once("./$libName/SessionUserData/sessionUserData.php");
-include_once("./$libName/settings.php");
-include_once("./$libName/init.php");
-include_once("./$libName/Error/error.php");
-include_once("./$libName/Error/errorLog.php");
-include_once("./$libName/Token/token.php");
-include_once("./$libName/Middlewares/index.php");
-include_once("./$libName/Redis/index.php");
-include_once("./$libName/Mailer/index.php");
-include_once("./$libName/Route/route.php");
+include_once(__DIR__ . "/$libName/SessionUserData/sessionUserData.php");
+include_once(__DIR__ . "/$libName/settings.php");
+include_once(__DIR__ . "/$libName/init.php");
+include_once(__DIR__ . "/$libName/Error/error.php");
+include_once(__DIR__ . "/$libName/Error/errorLog.php");
+include_once(__DIR__ . "/$libName/Token/token.php");
+include_once(__DIR__ . "/$libName/Middlewares/index.php");
+include_once(__DIR__ . "/$libName/Redis/index.php");
+include_once(__DIR__ . "/$libName/Mailer/index.php");
+include_once(__DIR__ . "/$libName/Route/route.php");
