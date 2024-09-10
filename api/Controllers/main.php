22f28ef588ea2648790f0a0567d51a34cd295f07
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-10 17:31:09 +0200

    Update main.php

--- api/Controllers/main.php
+++ api/Controllers/main.php
@@ -1,5 +1,5 @@
 <?php
 
-include_once ("./Controllers/authController.php");
-include_once ("./Controllers/htmlController.php");
-include_once ("./Controllers/cmdController.php");
+include_once (__DIR__ . "/Controllers/authController.php");
+include_once (__DIR__ . "/Controllers/htmlController.php");
+include_once (__DIR__ . "/Controllers/cmdController.php");
