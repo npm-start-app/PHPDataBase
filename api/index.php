d83e296a9a4d169f639719e2618ea1a60d859260
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-10 17:30:32 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,9 +2,9 @@
 use WBlib\Route;
 
 include_once (__DIR__ . "/lib/main.php");
-include_once ("./Roles/roles.php");
-include_once ("./Middlewares/main.php");
-include_once ("./Controllers/main.php");
-include_once ("./Routes/routes.php");
+include_once (__DIR__ . "/Roles/roles.php");
+include_once (__DIR__ . "/Middlewares/main.php");
+include_once (__DIR__ . "/Controllers/main.php");
+include_once (__DIR__ . "/Routes/routes.php");
 
 Route::route();
