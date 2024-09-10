ed69328d6ba50ab253f7e4c6d35be651e1bb2209
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-10 21:29:29 +0200

    Update routes.php

--- api/Routes/routes.php
+++ api/Routes/routes.php
@@ -8,7 +8,7 @@ use WBlib\Settings;
 
 if (array_key_exists('1', explode('/', $_SERVER['REQUEST_URI']))) {
     if (explode('/', $_SERVER['REQUEST_URI'])[1] === 'static') {
-        include_once (__DIR__ . '/..' . $_SERVER['REQUEST_URI']);
+        echo file_get_contents(__DIR__ . '/..' . $_SERVER['REQUEST_URI']);
 
         exit();
     } 
