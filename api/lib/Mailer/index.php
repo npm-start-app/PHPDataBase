efb0dc76f41a90f50ba0856f3d2e343fd93371f8
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-10 17:38:51 +0200

    Update index.php

--- api/lib/Mailer/index.php
+++ api/lib/Mailer/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once (__DIR__ . "/$libName/Mailer/Exception.php");
-include_once (__DIR__ . "/$libName/Mailer/SMTP.php");
-include_once (__DIR__ . "/$libName/Mailer/PHPMailer.php");
-include_once (__DIR__ . "/$libName/Mailer/Mailer.php");
+include_once ($_dir_ . "/$libName/Mailer/Exception.php");
+include_once ($_dir_ . "/$libName/Mailer/SMTP.php");
+include_once ($_dir_ . "/$libName/Mailer/PHPMailer.php");
+include_once ($_dir_ . "/$libName/Mailer/Mailer.php");
