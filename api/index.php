c4b350fcf3377ad64c4a4beea0c2e71c8159efae
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-10 17:26:53 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 use WBlib\Route;
 
-include_once ("./lib/main.php");
+include_once (__DIR__ . "/lib/main.php");
 include_once ("./Roles/roles.php");
 include_once ("./Middlewares/main.php");
 include_once ("./Controllers/main.php");
